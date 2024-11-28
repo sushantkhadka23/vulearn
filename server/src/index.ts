32f3ad7a597273d0a694e0ad5f5b4f83362baa08
aayush2561 <aayushdhakal0051@gmail.com>
2024-11-28 19:40:38 +0545

    sql injection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import Nosql from './A03-Injection/no-sql-injection/user.route';
-import Cmdinjection from './A03-Injection/command-injection/command-route'
+import Cmdinjection from './A03-Injection/command-injection/command.route'
+import SqlInjection from './A03-Injection/sql-injection/sqluser.route'
 import cors from 'cors'; 
 const app = express();
 
@@ -12,7 +13,8 @@ app.use(
 );
 app.use(express.json());
 app.use('/api/v1/no-sql',Nosql);
-app.use('/api/v1/cmdi',Cmdinjection)
+app.use('/api/v1/cmdi',Cmdinjection);
+app.use('/api/v1/sqli',SqlInjection);
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
