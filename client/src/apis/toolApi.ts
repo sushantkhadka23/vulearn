6d901492767b07e1b3e183278618a919abf5515c
sushantkhadka23 <sushantkhadka23@proton.me>
2025-01-18 16:54:27 +0545

    added toolApi function

--- client/src/apis/toolApi.ts
+++ client/src/apis/toolApi.ts
@@ -1,4 +1,16 @@
+import axios from "axios";
+import { ToolTopics } from "../models/tools";
+``
+const toolEndpoint: string = ``;
 
+const toolApi = async (): Promise<ToolTopics | null> => {
+  try {
+    const response = await axios.get<ToolTopics>(toolEndpoint);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
+};
 
-//aayush kailey?
-
+export { toolApi };
