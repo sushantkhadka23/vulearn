66d91b35f3da8e2d32c4cd72289a1eb6b23aa11b
sushantkhadka23 <sushantkhadka23@proton.me>
2025-01-26 14:11:29 +0545

    added ssrf

--- server/src/lab/A10-SSRF/ssrf.route.ts
+++ server/src/lab/A10-SSRF/ssrf.route.ts
@@ -4,6 +4,6 @@ import { fetchapi, adminsafeapi } from './ssrf.controller';
 const router = express.Router();
 
 router.get('/fetch-api',fetchapi as any);
-router.get('/admin' , adminsafeapi as any)
+router.get('/admin' , adminsafeapi as any);
 
 export default router;
